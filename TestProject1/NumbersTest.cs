64142c6d9ada8b395c6a8b68990f4f79e90cd540
Madde <105611442+SpiritualMaddie@users.noreply.github.com>
2023-09-26 15:37:57 +0200

    Add project files.

---
+++ TestProject1/NumbersTest.cs
@@ -0,0 +1,36 @@
+using WebApp;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class NumbersTest
+    {
+        [TestMethod]
+        public void VariablesTest()
+        {
+            // Arrange
+            Numbers num = new Numbers();
+            var actual = num.Add(10, 12);
+            var expected = 22;
+
+            // Act
+            Assert.AreEqual(expected, actual);
+
+            // Assert
+        }
+
+        [TestMethod]
+        public void VariablesTest2()
+        {
+            // Arrange
+            Numbers num = new Numbers();
+            var actual = num.Add(11, 12);
+            var expected = 22;
+
+            // Act
+            Assert.AreEqual(expected, actual);
+
+            // Assert
+        }
+    }
+}
---
+++ WebApp/Numbers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp
+{
+    public class Numbers
+    {
+        public int Add(int x, int y)
+        {
+            string name = "Madde";
+            return x + y;
+        }
+    }
+}
